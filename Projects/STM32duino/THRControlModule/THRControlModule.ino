dcbbab379b8dd1837d1c6f02d9de2865412dd20f
TsutsumiHiroki <hiroki1815@outlook.jp>
2023-05-11 22:09:06 +0900

    温度取得、サーボ制御の実装

--- Projects/STM32duino/THRControlModule/THRControlModule.ino
+++ Projects/STM32duino/THRControlModule/THRControlModule.ino
@@ -1,41 +1,54 @@
+#include <TaskManager.h>
 #include "IcsHardSerialClass.h"
-// #include "EX_MAX31855.h" //test用のhファイル
+#include "EX_MAX31855.h" //test用のhファイル
 // #include "HardwareSerial.h"
 
 /*B3M Servo Config START*/
-const byte EN_PIN = 3;
+const byte EN_PIN = 2;
 const long BAUNDRATE = 115200;
-const int TIMEOUT = 1000;
+// + 1152+100;
+const int TIMEOUT = 500;
 // HardwareSerial SerialX(RX, TX); X = 1, 2, 3...
 // HardwareSerial Serial1(PA_10, PA_9); // STM32F303K8
+// HardwareSerial Serial1(0, 1); // STM32F303K8
 
-//Everyくんを搭載する可能性大
+// Everyくんを搭載する可能性大
 
 IcsHardSerialClass B3M(&Serial1, EN_PIN, BAUNDRATE, TIMEOUT);
 /*B3M Servo Config END*/
 
 /*Position Change Config START*/
-constexpr int POSITION_CHANGING_THRESHOLD = 10;
+constexpr int POSITION_CHANGING_THRESHOLD = 50;
 int Launch_Count = 0;
 int Waiting_Count = 0;
 int Position = 1;
+int WaitingPin = 4;
+int LaunchPin = 5;
 /*Position Change Config END*/
 
 /*MAX31855 Config START*/
-// int32_t rawData = 0;
-// MAX31855 myMAX31855(3);
+int32_t rawData = 0;
+MAX31855 myMAX31855(3);
 /*MAX31855 Config END*/
+
+/*MAX31855 Pin Config*/
+// MAX31855      Every
+// CLK  -------- SCK(D13)
+// CS   -------- CS_Pin(D3)
+// DO   -------- CIPO(D12)
+// GND  -------- GND
+// 3Vo  -------- N/C
+// Vin  -------- 3~5V
 
 void setup()
 {
-    pinMode(8, INPUT_PULLUP);
-    pinMode(9, INPUT_PULLUP);
+    pinMode(WaitingPin, INPUT_PULLUP);
+    pinMode(LaunchPin, INPUT_PULLUP);
 
     B3M.begin(); // B3Mと通信開始
-    pinMode(13, OUTPUT);
 
     Serial1.begin(115200, SERIAL_8N1); // 通信速度、パリティなしに設定
-    // Serial2.begin(115200);
+    //  + 1152+100
     Serial.begin(115200);
 
     B3MwriteCommand(0x01, 0x02, 0x28); // 動作モード：Free
@@ -54,32 +67,35 @@ void setup()
     LED_Blink();
     delay(1000);
 
-    B3MsetPosition(0x01, 4500, 100);
+    B3MsetPosition(0x01, 4500, 1000);
 
-    // myMAX31855.begin();
-    // while (myMAX31855.getChipID() != MAX31855_ID)
-    // {
-    //     Serial.println(F("MAX31855 error"));
-    //     delay(5000);
-    // }
+    myMAX31855.begin();
+    while (myMAX31855.getChipID() != MAX31855_ID)
+    {
+        Serial.println(F("MAX31855 error"));
+        delay(5000);
+    }
 
-    // Serial.println("");
-    // Serial.println(F("MAX31855 GO"));
+    Tasks.add("MAX31855_getTemperature", []()
+              {
+                    Serial.print(myMAX31855.getTemperature(rawData));
+                    Serial.print(", ");
+                    Serial.print(myMAX31855.getColdJunctionTemperature(rawData));
+                    Serial.print(", ");
+                    Serial.print(Launch_Count);
+                    Serial.print(", ");
+                    Serial.println(Waiting_Count);
+     })
+        ->startIntervalMsec(5);
 }
 
 void loop()
 {
-    // MAX31855Errornotification(); //MAX31855 のエラーをお知らせ
+    Tasks.update();
 
-    // rawData = myMAX31855.readRawData();
+    rawData = myMAX31855.readRawData();
 
-    // Serial.print(F("ColdJuncction = "));
-    // Serial.println(myMAX31855.getColdJunctionTemperature(rawData));
-
-    // Serial.print(F("ThermoCouple = "));
-    // Serial.println(myMAX31855.getTemperature(rawData));
-
-    if (Position == 1 && digitalRead(8) == LOW)
+    if (Position == 1 && digitalRead(LaunchPin) == LOW)
     {
         Launch_Count++;
     }
@@ -90,18 +106,15 @@ void loop()
 
     if (Launch_Count >= POSITION_CHANGING_THRESHOLD)
     {
-        Launch_Count = 0;
         B3MsetPosition(0x01, 4500, 100);
-        digitalWrite(13, HIGH);
-        // Serial.println(Launch_Count);
+        Launch_Count = 0;
         Position = 2;
         delay(50);
     }
-    // Serial.println(Launch_Count);
 
     //-------------------------------------------------//
 
-    if (Position == 2 && digitalRead(9) == LOW)
+    if (Position == 2 && digitalRead(WaitingPin) == LOW)
     {
         Waiting_Count++;
     }
@@ -114,24 +127,39 @@ void loop()
     {
         Waiting_Count = 0;
         B3MsetPosition(0x01, -4500, 100);
-        digitalWrite(13, LOW);
-        // Serial.println(Waiting_Count);
         Position = 1;
         delay(50);
     }
-    //  Serial.println(Waiting_Count);
+
+    // Serial.print(rawData);
+    // Serial.print(", ");
+
+    MAX31855Errornotification(); // MAX31855 のエラーをお知らせ
+    FillingConfirmation();
+
+    // rawData = myMAX31855.readRawData();
+
+    // Serial.print("| ColdJuncction |");
+    // Serial.print(myMAX31855.getColdJunctionTemperature(rawData));
+    // Serial.print(",");
+    // Serial.print("| ThermoCouple |");
+    // Serial.print(myMAX31855.getTemperature(rawData));
+    // Serial.print(",");
+    // Serial.print(Launch_Count);
+    // Serial.print(",");
+    // Serial.println(Waiting_Count);
 
     //---テスト用---//
     // Positionで動作角指定
 
     // B3MsetPosition(0x01, 9000, 5000);
     // delay(1000);
-    // // Serial.println("OPEN");
-    // // Serial1.println("Hello");
+    // Serial.println("OPEN");
+    // Serial1.println("Hello");
     // B3MsetPosition(0x01, -9000, 5000);
     // delay(1000);
-    // // Serial.println("CLOSE");
-    // // Serial1.println("TSRP");
+    // Serial.println("CLOSE");
+    // Serial1.println("TSRP");
 }
 
 int B3MwriteCommand(byte id, byte TxData, byte Address)
@@ -155,8 +183,10 @@ int B3MwriteCommand(byte id, byte TxData, byte Address)
     for (int i = 0; i < 7; i++)
     {
         txCmd[7] += txCmd[i];
-        Serial.println(txCmd[i]);
+        Serial.print(txCmd[i], HEX);
+        Serial.print(" ");
     }
+    Serial.println("");
     txCmd[7] = (byte)(txCmd[7]); // CHECKSUM
 
     // flag = B3M.synchronize(txCmd, sizeof txCmd, rxCmd, sizeof rxCmd);
@@ -164,6 +194,7 @@ int B3MwriteCommand(byte id, byte TxData, byte Address)
 
     if (flag == false)
     {
+        Serial.println("ERROR");
         return -1;
     }
     reData = rxCmd[2];
@@ -210,45 +241,48 @@ int B3MsetPosition(byte id, int Pos, int Time)
         Serial.print(reData, HEX);
         Serial.print(" ");
     }
+    Serial.println("");
 
     return reData;
 }
 
-// int B3MreadPOSITION(byte id, byte RxData, byte Address)
-// {
-//     byte txCmd[7];
-//     byte rxCmd[5];
-// }
+void MAX31855Errornotification()
+{
+    while (myMAX31855.detectThermocouple() != MAX31855_THERMOCOUPLE_OK)
+    {
+        switch (myMAX31855.detectThermocouple())
+        {
+        case MAX31855_THERMOCOUPLE_SHORT_TO_VCC:
+            Serial.println(F("Thermocouple short to VCC"));
+            break;
 
-// void MAX31855Errornotification()
-// {
-//     while (myMAX31855.detectThermocouple() != MAX31855_THERMOCOUPLE_OK)
-//     {
-//         switch (myMAX31855.detectThermocouple())
-//         {
-//         case MAX31855_THERMOCOUPLE_SHORT_TO_VCC:
-//             Serial.println(F("Thermocouple short to VCC"));
-//             break;
+        case MAX31855_THERMOCOUPLE_SHORT_TO_GND:
+            Serial.println(F("Thermocouple short to GND"));
+            break;
 
-//         case MAX31855_THERMOCOUPLE_SHORT_TO_GND:
-//             Serial.println(F("Thermocouple short to GND"));
-//             break;
+        case MAX31855_THERMOCOUPLE_NOT_CONNECTED:
+            Serial.println(F("Thermocouple not connected"));
+            break;
 
-//         case MAX31855_THERMOCOUPLE_NOT_CONNECTED:
-//             Serial.println(F("Thermocouple not connected"));
-//             break;
+        case MAX31855_THERMOCOUPLE_UNKNOWN:
+            Serial.println(F("Thermocouple unknown error"));
+            break;
 
-//         case MAX31855_THERMOCOUPLE_UNKNOWN:
-//             Serial.println(F("Thermocouple unknown error"));
-//             break;
+        case MAX31855_THERMOCOUPLE_READ_FAIL:
+            Serial.println(F("Thermocouple read error, check chip & spi cable"));
+            break;
+        }
+    }
+}
 
-//         case MAX31855_THERMOCOUPLE_READ_FAIL:
-//             Serial.println(F("Thermocouple read error, check chip & spi cable"));
-//             break;
-//         }
-//         delay(5000);
-//     }
-// }
+void FillingConfirmation()
+{
+    // Serial.print(myMAX31855.getTemperature(rawData));
+    if (myMAX31855.getTemperature(rawData) >= 30.00)
+    {
+        Serial.println("COMPLETED");
+    }
+}
 
 void LED_Blink()
 {
