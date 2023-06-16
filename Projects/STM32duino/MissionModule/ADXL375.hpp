f05590c41e83595c18dedca4eab267cd507754aa
waiwai2525 <waiwai.cps@gmail.com>
2023-06-16 17:14:30 +0900

    ADXLを抽象化ライブラリに分割

--- Projects/STM32duino/MissionModule/ADXL375.hpp
+++ Projects/STM32duino/MissionModule/ADXL375.hpp
@@ -3,16 +3,11 @@
 
 #include <Arduino.h>
 #include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_ADXL375.h>
 
 
 class ADXL375 {
 public:
   void begin();
 
-  float get();
-
-private:
-  Adafruit_ADXL375 accel = Adafruit_ADXL375(-1, &Wire);
+  void getAcceleration(float* x, float* y, float* z);
 };
--- Projects/STM32duino/MissionModule/MissionModule.ino
+++ Projects/STM32duino/MissionModule/MissionModule.ino
@@ -6,7 +6,7 @@
 #include <mcp2515_can.h>
 #include "PullupPin.hpp"
 #include "OutputPin.hpp"
-// #include "ADXL375.hpp"
+#include "ADXL375.hpp"
 #include "Sd.hpp"
 
 
@@ -40,7 +40,7 @@ namespace timer {
 }
 
 namespace sensor {
-  // ADXL375 adxl;
+  ADXL375 adxl;
 }
 
 namespace recorder {
@@ -96,64 +96,7 @@ void setup() {
   Wire.begin();
   Wire.setClock(400000);
 
-  // ADXL ///////////////////////////////////////////////////////////////////////////
-
-  // Disable interrupts to start
-  Wire.beginTransmission(0x53);
-  Wire.write(0x2E);
-  Wire.write(0);
-  Wire.endTransmission();
-
-  // 62.5 mg/LSB (so 0xFF = 16 g)
-  Wire.beginTransmission(0x53);
-  Wire.write(0x1D);
-  Wire.write(20);
-  Wire.endTransmission();
-
-  // Max tap duration, 625 µs/LSB
-  Wire.beginTransmission(0x53);
-  Wire.write(0x21);
-  Wire.write(50);
-  Wire.endTransmission();
-
-  // Tap latency, 1.25 ms/LSB, 0=no double tap
-  Wire.beginTransmission(0x53);
-  Wire.write(0x22);
-  Wire.write(0);
-  Wire.endTransmission();
-
-  // Waiting period,  1.25 ms/LSB, 0=no double tap
-  Wire.beginTransmission(0x53);
-  Wire.write(0x23);
-  Wire.write(0);
-  Wire.endTransmission();
-
-  // Enable the XYZ axis for tap
-  Wire.beginTransmission(0x53);
-  Wire.write(0x2A);
-  Wire.write(0x7);
-  Wire.endTransmission();
-
-  // Enable measurements
-  Wire.beginTransmission(0x53);
-  Wire.write(0x2D);
-  Wire.write(0x08);
-  Wire.endTransmission();
-
-  // Force full range (fixes issue with DATA_FORMAT register's reset value)
-  // Per datasheet, needs to be D4=0, D3=D1=D0=1
-  Wire.beginTransmission(0x53);
-  Wire.write(0x31);
-  Wire.write(0b00001011);
-  Wire.endTransmission();
-
-  // Sets the data rate for the ADXL343(controls power consumption)
-  Wire.beginTransmission(0x53);
-  Wire.write(0x2C);
-  Wire.write(0b1110);
-  Wire.endTransmission();
-
-  ///////////////////////////////////////////////////////////////////////////////////
+  sensor::adxl.begin();
 
   canbus::initialize();
 
@@ -230,23 +173,11 @@ void timer::task50Hz() {
 
 
 void timer::task1k2Hz() {
-  Wire.beginTransmission(0x53);
-  Wire.write(0x32);
-  Wire.endTransmission();
-  Wire.requestFrom(0x53, 6);
+  sensor::adxl.getAcceleration(&data::acceleration_x, &data::acceleration_y, &data::acceleration_z);
 
-  int16_t xRaw = ((int16_t)Wire.read() << 8) | (int16_t)Wire.read();
-  data::acceleration_x = (float)xRaw * 0.049;
-
-  int16_t yRaw = ((int16_t)Wire.read() << 8) | (int16_t)Wire.read();
-  data::acceleration_y = (float)yRaw * 0.049;
-
-  int16_t zRaw = ((int16_t)Wire.read() << 8) | (int16_t)Wire.read();
-  data::acceleration_z = (float)zRaw * 0.049;
-
-  // Serial.print("x:"); Serial.print(data::acceleration_x / 9.8);
-  // Serial.print(",y:"); Serial.print(data::acceleration_y / 9.8);
-  // Serial.print(",z:"); Serial.println(data::acceleration_z / 9.8);
+  Serial.print("x:"); Serial.print(data::acceleration_x / 9.8);
+  Serial.print(",y:"); Serial.print(data::acceleration_y / 9.8);
+  Serial.print(",z:"); Serial.println(data::acceleration_z / 9.8);
 }
 
 
