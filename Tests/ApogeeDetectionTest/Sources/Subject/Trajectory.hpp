079371a2b33e8171a7f2d3095959c65496189a71
waiwai2525 <waiwai.cps@gmail.com>
2023-07-07 20:38:53 +0900

    頂点検知試験用のコードを追加

---
+++ Tests/ApogeeDetectionTest/Sources/Subject/Trajectory.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+
+#include <Arduino.h>
+#include "ExponentialMovingAverage.hpp"
+
+
+class Trajectory {
+private:
+  ExponentialMovingAverage* _altitudeAverageStrong;
+  ExponentialMovingAverage* _altitudeAverageWeak;
+
+  float _referencePressure = 1013.25;
+
+public:
+  Trajectory(float sensitivityStrong, float sensitivityWeak);
+
+  void setReferencePressure(float referencePressure);
+
+  float update(float pressure, float temperature);
+  float update(float altitude);
+
+  float climbIndex();
+  bool isFalling();
+};
