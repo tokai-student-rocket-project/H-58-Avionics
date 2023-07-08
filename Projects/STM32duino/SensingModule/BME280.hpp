bb587ef45d106e0cb0234f9355b80827123aee21
waiwai2525 <waiwai.cps@gmail.com>
2023-07-08 20:05:21 +0900

    💡 BMEにコメントを追加

--- Projects/STM32duino/SensingModule/BME280.hpp
+++ Projects/STM32duino/SensingModule/BME280.hpp
@@ -6,11 +6,15 @@
 #include <SparkFunBME280.h>
 
 
+/// @brief BME280の抽象化クラス
 class BME {
 public:
+  /// @brief センサを初期化して起動する
   void begin();
 
-  void getPressure(float* pressure);
+  /// @brief 現在の気圧を返す
+  /// @param pressure_hPa 気圧のポインタ [hPa]
+  void getPressure(float* pressure_hPa);
 
 private:
   BME280 _bme280;
