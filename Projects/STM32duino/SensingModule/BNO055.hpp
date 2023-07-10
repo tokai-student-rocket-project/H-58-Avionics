6ca955eedc5a3f155d30bc1fa7058b48da335df3
waiwai2525 <waiwai.cps@gmail.com>
2023-07-10 13:38:51 +0900

    🚧 バッテリー切れ(死)

--- Projects/STM32duino/SensingModule/BNO055.hpp
+++ Projects/STM32duino/SensingModule/BNO055.hpp
@@ -5,6 +5,8 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
+#include "Vector3D.hpp"
+
 
 /// @brief BME055の抽象化クラス
 class BNO055 {
@@ -14,35 +16,24 @@ public:
 
 
   /// @brief 現在の加速度ベクトルを返す
-  /// @param x x軸加速度のポインタ [mps2]
-  /// @param y y軸加速度のポインタ [mps2]
-  /// @param z y軸加速度のポインタ [mps2]
-  void getAcceleration(float* x, float* y, float* z);
+/// @param acceleration_mps2 加速度ベクトルのポインタ [mps2]
+  void getAcceleration(Vector3D* acceleration_mps2);
 
   /// @brief 現在の地磁気を返す
-  /// @param x x軸地磁気のポインタ [nT]
-  /// @param y y軸地磁気のポインタ [nT]
-  /// @param z y軸地磁気のポインタ [nT]
-  void getMagnetometer(float* x, float* y, float* z);
-
+  /// @param magnetometer_nT 地磁気ベクトルのポインタ [nT]
+  void getMagnetometer(Vector3D* magnetometer_nT);
 
   /// @brief 現在の角加速度を返す
-  /// @param x x軸周り角加速度のポインタ [dps]
-  /// @param y y軸周り角加速度のポインタ [dps]
-  /// @param z y軸周り角加速度のポインタ [dps]
-  void getGyroscope(float* x, float* y, float* z);
+  /// @param gyroscope_dps 加速度のポインタ [dps]
+  void getGyroscope(Vector3D* gyroscope_dps);
 
   /// @brief 現在の重力加速度を返す
-  /// @param x x軸重力加速度のポインタ [mps2]
-  /// @param y y軸重力加速度のポインタ [mps2]
-  /// @param z y軸重力加速度のポインタ [mps2]
-  void getGravityVector(float* x, float* y, float* z);
+  /// @param gravity_mps2 重力加速ベクトルのポインタ [mps2]
+  void getGravityVector(Vector3D* gravity_mps2);
 
   /// @brief 現在の線形加速度を返す
-  /// @param x x軸線形加速度のポインタ [mps2]
-  /// @param y y軸線形加速度のポインタ [mps2]
-  /// @param z y軸線形加速度のポインタ [mps2]
-  void getLinearAcceleration(float* x, float* y, float* z);
+  /// @param x 線形加速度ベクトルのポインタ [mps2]
+  void getLinearAcceleration(Vector3D* linear_acceleration_mps2);
 
   /// @brief 現在の姿勢角を返す
   /// @param x x軸(Roll)姿勢角のポインタ [deg]
---
+++ Projects/STM32duino/SensingModule/Vector3D.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+
+#include <Arduino.h>
+
+
+/// @brief 3次元ベクトル
+struct Vector3D {
+public:
+  float x;
+  float y;
+  float z;
+};
