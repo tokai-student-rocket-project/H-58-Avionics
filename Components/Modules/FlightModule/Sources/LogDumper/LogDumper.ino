52db2b666c35a43400706d9e60a1e0644b208280
waiwai2525 <waiwai.cps@gmail.com>
2023-08-11 01:44:39 +0900

    ログダンパーをバルブ情報に対応

--- Components/Modules/FlightModule/Sources/LogDumper/LogDumper.ino
+++ Components/Modules/FlightModule/Sources/LogDumper/LogDumper.ino
@@ -14,12 +14,15 @@ void setup() {
   SPI.setSCLK(A4);
   SPI.begin();
 
-  // TODO 対応
   MsgPacketizer::subscribe_manual(0xAA,
     [&](uint32_t millis, uint16_t flightTime,
       uint8_t flightMode, bool cameraState, bool sn3State, bool doLogging,
       bool isFalling, bool flightPinState, bool resetPinState,
-      float supplyVoltage, float batteryVoltage, float poolVoltage) {
+      float supplyVoltage, float batteryVoltage, float poolVoltage,
+      bool isLaunchMode,
+      float motorTemperature, float mcuTemperature, float current, float inputVoltage,
+      float currentPosition, float currentDesiredPosition, float currentVelocity
+      ) {
         Serial.print(millis); Serial.print(",");
         Serial.print(flightTime); Serial.print(",");
         Serial.print(flightMode); Serial.print(",");
@@ -31,7 +34,15 @@ void setup() {
         Serial.print(resetPinState); Serial.print(",");
         Serial.print(supplyVoltage); Serial.print(",");
         Serial.print(batteryVoltage); Serial.print(",");
-        Serial.print(poolVoltage); Serial.println();
+        Serial.print(poolVoltage); Serial.print(",");
+        Serial.print(isLaunchMode); Serial.print(",");
+        Serial.print(motorTemperature); Serial.print(",");
+        Serial.print(mcuTemperature); Serial.print(",");
+        Serial.print(current); Serial.print(",");
+        Serial.print(inputVoltage); Serial.print(",");
+        Serial.print(currentPosition); Serial.print(",");
+        Serial.print(currentDesiredPosition); Serial.print(",");
+        Serial.print(currentVelocity); Serial.println();
     }
   );
 
